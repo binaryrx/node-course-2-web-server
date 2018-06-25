642d1d4dc4014183268191f5c85017cad65ae820
BinaryRx <Binaryrx@gmail.com>
2018-06-25 09:08:13 +0300

    index ->Index.hbs

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
+app.set('views', __dirname +'/views');
 app.set('view engine','hbs');
 
 
